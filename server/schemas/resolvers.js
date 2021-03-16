dfa4fff91a1ff265dff4b57c209fc3fe633bd674
BeatyPete <akira941@gmail.com>
2021-03-16 01:00:13 -0500

    setup resolvers and typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,39 +1,24 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('Books')
+    },
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
           .select('-__v -password')
-          .populate('thoughts')
-          .populate('friends');
+          .populate('savedBooks');
 
         return userData;
       }
 
       throw new AuthenticationError('Not logged in');
-    },
-    users: async () => {
-      return User.find()
-        .select('-__v -password')
-        .populate('thoughts')
-        .populate('friends');
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username })
-        .select('-__v -password')
-        .populate('friends')
-        .populate('thoughts');
-    },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    thought: async (parent, { _id }) => {
-      return Thought.findOne({ _id });
     }
   },
 
@@ -60,46 +45,32 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    addThought: async (parent, args, context) => {
-      if (context.user) {
-        const thought = await Thought.create({ ...args, username: context.user.username });
-
-        await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $push: { thoughts: thought._id } },
-          { new: true }
-        );
-
-        return thought;
-      }
-
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
-      if (context.user) {
-        const updatedThought = await Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          { $push: { reactions: { reactionBody, username: context.user.username } } },
+    saveBook: async (parent, {bookId, authors, description, title, image, link}, context) => {
+      /* if (context.user) { */
+        console.log(authors)
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: /* context.user._id */ "604ef7330cbe233330401437" },
+          { $push: { savedBooks: { bookId: bookId, authors: authors, description: description, title: title, image: image, link: link  } } },
           { new: true, runValidators: true }
         );
 
-        return updatedThought;
-      }
+        return updatedUser;
+      /* }
 
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError('You need to be logged in!'); */
     },
-    addFriend: async (parent, { friendId }, context) => {
-      if (context.user) {
+    removeBook: async (parent, {bookId}, context) => {
+      /* if (context.user) { */
         const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { friends: friendId } },
+          { _id: /* context.user._id */ "604ef7330cbe233330401437" },
+          { $pull: { savedBooks: { bookId: bookId} } },
           { new: true }
-        ).populate('friends');
+        );
 
         return updatedUser;
-      }
+      /* }
 
-      throw new AuthenticationError('You need to be logged in!');
+      throw new AuthenticationError('You need to be logged in!'); */
     }
   }
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,51 +1,37 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type User {
-    _id: ID
-    username: String
-    email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
-  }
 
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
-
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-  }
-
-  type Auth {
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
     token: ID!
     user: User
   }
 
-  type Query {
-    me: User
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
-  }
-
-  type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addReaction(thoughtId: ID!, reactionBody: String!): Thought
-    addFriend(friendId: ID!): User
-  }
+    type Query {
+        users: [User]
+        me:User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookId: String! authors: [String] description: String! title: String! image: String! link: String!): User 
+        removeBook(bookId: String!): User
+    }
 `;
 
 module.exports = typeDefs;
